824abd2ec987d7fc65348d59482f33e8b980b405
derKanzlerin <alexander.bahr86@gmx.de>
2021-01-05 21:14:30 +0800

    Finish log in, fist try for save and reload post

--- main.js
+++ main.js
@@ -8,24 +8,38 @@ function showpostinput() {
     document.getElementById('alert-sign-in').classList.add('d-none');
 }
 
-function post() {
-    let post = document.getElementById('post').value;
-    document.getElementById('firstpost').innerHTML += `<div class="card text-center m-3">
-<div class="card-header text-left bg-p-hd-fo">
-    SEE! This is important for you:
-</div>
-<div class="card-body bg-text-post">
-    <p class="card-text ">${post}</p>
 
-</div>
-<div class="card-footer text-muted bg-p-hd-fo">
-    ${userxy} ago
-</div>
-</div>`;
+function post() {
+    let newpost = document.getElementById('post').value;
+    userpost.push(newpost);
+    printpost(newpost)
     document.getElementById('post').value = '';
+    removearray(`${userxy}`);
+    setarray(`${userxy}`, userpost);
 }
 
+function printpost() {
+    document.getElementById('firstpost').innerHTML = '';
+    for (i = 0; i < userpost.length; i++) {
+        console.log(i);
+        document.getElementById('firstpost').innerHTML += `<div class="card text-center m-3">
+    <div class="card-header text-left bg-p-hd-fo">
+        SEE! This is important for you:
+    </div>
+    <div class="card-body bg-text-post">
+        <p class="card-text ">${userpost[i]}</p>
+    
+    </div>
+    <div class="card-footer text-muted bg-p-hd-fo">
+        ${userxy} ago
+    </div>
+    </div>`;
+    };
+}
+
+
 // variablen and größen
+let userpost = [];
 
 let user = [];
 
@@ -41,23 +55,46 @@ function savename() {
 
 // return and log in
 
-function loaduser() {
-    storageuser = getarray('user')
-    console.log(storageuser);
-    varenty = storageuser.find(searchuser);
-    if (varenty === undefined) {
+function checkuser() {
+    storageuser = getarray('user');
+    if (!storageuser) {
+        document.getElementById('registry-input').value = '';
         alert('PLS registry');
     } else {
-        showpostinput();
+        loaduser();
     };
     document.getElementById('login-input').value = '';
 }
+
+function loaduser() {
+    storageuser = getarray('user');
+    varenty = storageuser.find(searchuser);
+    if (varenty === undefined) {
+        alert('PLS registry');
+    }
+    else {
+        showpostinput();
+        loadpost();
+    };
+}
+
+// Load Posts
+
+function loadpost() {
+    postsxy = getarray(`${userxy}`);
+    userpost.push(postsxy);
+    console.log(userpost);
+    printpost();
+}
+
+
 
 function logout() {
     document.getElementById('sign-in-btn').classList.remove('d-none');
     document.getElementById('logout-button').classList.add('d-none');
     document.getElementById('post-input').classList.add('d-none');
     document.getElementById('alert-sign-in').classList.remove('d-none');
+    userpost = [];
 }
 
 function searchuser(storageuser) {
@@ -74,6 +111,10 @@ function searchuser(storageuser) {
 // }
 
 // save array / get Arry
+
+function removearray(key) {
+    localStorage.removeItem(key);
+}
 
 function setarray(key, array) {
     localStorage.setItem(key, JSON.stringify(array));
